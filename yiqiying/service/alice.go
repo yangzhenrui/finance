d6191cfcf9f9c3ca21d5bf034aaeb4b045714e01
ade <798449992@qq.com>
2022-11-24 17:52:04 +0800

    结账bugfix

--- yiqiying/service/alice.go
+++ yiqiying/service/alice.go
@@ -1,19 +1,21 @@
 package service
 
 import (
-	"bytes"
 	"encoding/json"
 	"fmt"
+	"github.com/gookit/goutil/arrutil"
 	"github.com/yangzhenrui/finance/credential"
 	"github.com/yangzhenrui/finance/util"
 	"github.com/yangzhenrui/finance/yiqiying/context"
 	"io/ioutil"
 	"net/http"
+	"net/url"
 	"strconv"
+	"strings"
 )
 
 const (
-	// GetCloseInfo 结账信息接口
+	// GetCloseInfoUrl 结账信息接口
 	GetCloseInfoUrl = "https://openapi.17win.com/gateway/openyqdz/alice/closeInfo/getCloseInfo"
 )
 
@@ -26,7 +28,7 @@ func NewAlice(ctx *context.Context) *Alice {
 }
 
 func (c *Alice) setHeader(signature string, httpRequest *http.Request) {
-	httpRequest.Header.Set("Content-Type", "application/json")
+	httpRequest.Header.Set("Content-Type", "application/x-www-form-urlencoded")
 	httpRequest.Header.Set("version", c.Context.Version)
 	httpRequest.Header.Set("timestamp", strconv.FormatInt(c.Context.Timestamp, 10))
 	httpRequest.Header.Set("appKey", c.Context.Config.AppKey)
@@ -35,16 +37,12 @@ func (c *Alice) setHeader(signature string, httpRequest *http.Request) {
 }
 
 type GetCloseInfoRequest struct {
-	CustomerIds []string `json:"customerIds"` // 企业ID，最大支持500个
+	CustomerIds []int64 `json:"customerIds"` // 企业ID，最大支持500个
 }
 
 type GetCloseInfoResponse struct {
-	Head                     util.CommonError         `json:"head"`
-	GetCloseInfoResponseBody GetCloseInfoResponseBody `json:"body"`
-}
-
-type GetCloseInfoResponseBody struct {
-	GetCloseInfoList []GetCloseInfoList `json:"customerList"`
+	Head util.CommonError   `json:"head"`
+	Body []GetCloseInfoList `json:"body"`
 }
 
 type GetCloseInfoList struct {
@@ -57,10 +55,16 @@ type GetCloseInfoList struct {
 
 // GetCloseInfo 结账信息接口
 func (c *Alice) GetCloseInfo(req GetCloseInfoRequest) (result GetCloseInfoResponse, err error) {
-	aliceReq, err := json.Marshal(&req)
-	reader := bytes.NewReader(aliceReq)
-	httpRequest, err := http.NewRequest("POST", GetCloseInfoUrl, reader)
-	c.SignatureHandle = credential.NewDefaultSignature(nil, c.AppKey, c.AppSecret, c.CustomerId, nil, nil, nil, c.Timestamp, c.Version, c.XReqNonce, credential.CacheKeyYiQiYingPrefix, c.Cache)
+	customerIdsOfString := arrutil.AnyToString(req.CustomerIds)
+	customerIdsOfString = strings.ReplaceAll(customerIdsOfString, "[", "")
+	customerIdsOfString = strings.ReplaceAll(customerIdsOfString, "]", "")
+	postData := url.Values{}
+	postData.Add("customerIds", customerIdsOfString)
+
+	httpRequest, err := http.NewRequest("POST", GetCloseInfoUrl, strings.NewReader(postData.Encode()))
+
+	//httpRequest, err := http.NewRequest("POST", GetCloseInfoUrl, reader)
+	c.SignatureHandle = credential.NewDefaultSignature(nil, c.AppKey, c.AppSecret, nil, nil, nil, nil, c.Timestamp, c.Version, c.XReqNonce, credential.CacheKeyYiQiYingPrefix, c.Cache)
 	signature, err := c.GetSignature()
 	if err != nil {
 		return
